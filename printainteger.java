9ef44c0ebe5448cd4f56025526d623e2c1f4f877
javamudassir112 <87257204+javamudassir112@users.noreply.github.com>
2021-07-11 09:47:25 +0100

    Add files via upload
    2. Java Program to Print an Integer (Entered by the User)

---
+++ printainteger.java
@@ -0,0 +1,13 @@
+package Smweek2lab;
+
+import java.util.Scanner;
+
+public class printainteger {
+public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("please enter a number:.......");
+		int num=scan.nextInt();
+		System.out.println("the entered number is "+num);
+		
+		}
+}
