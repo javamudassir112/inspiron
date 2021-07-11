5d88c721dfd3e0f953a66a898f76461cef0796b5
javamudassir112 <87257204+javamudassir112@users.noreply.github.com>
2021-07-11 10:34:53 +0100

    Add files via upload
    5. Find the ASCII Value

---
+++ valueAascii.java
@@ -0,0 +1,9 @@
+package Smweek2lab;
+
+public class valueAascii {
+public static void main(String[] args) {
+char ch = 'M';
+	int ascii = ch;
+		System.out.println("The ASCII value of " + ch + " is: " + ascii);
+		}
+}
