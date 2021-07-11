69cbecf7b8cb0a960d828cc7bcde08cbf8f2b870
javamudassir112 <87257204+javamudassir112@users.noreply.github.com>
2021-07-11 07:56:32 +0100

    Add files via upload
    1. Java Program to Add Two Numbers

---
+++ addtwonumber.java
@@ -0,0 +1,11 @@
+package Smweek2lab;
+
+public class addtwonumber {
+public static void main(String[] args) {
+		int a = 100;
+	int b = 200;
+	int sum = a + b;
+	System.out.println("sum of a and b= " +sum);
+	
+	}
+	}
