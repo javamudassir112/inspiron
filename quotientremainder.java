556da53127d87503e254b84e41b9b0f32bfd8c37
javamudassir112 <87257204+javamudassir112@users.noreply.github.com>
2021-07-11 11:02:16 +0100

    Add files via upload
    6A. Computer Quotient and Remainder

---
+++ quotientremainder.java
@@ -0,0 +1,31 @@
+package Smweek2lab;
+
+import java.util.Scanner;
+
+public class quotientremainder {
+public static void main(String[] args) {
+	
+	int dividend = 25, divisor = 4;
+	int quotient = dividend / divisor;
+	int remainder = dividend % divisor;
+	System.out.println("Quotient = " + quotient);
+	System.out.println("Remainder = " + remainder);
+	
+	
+	// why ther is no result with this number??????????
+	//int dividend;
+	//Scanner input = new Scanner(System.in);
+	//dividend = input.nextInt();
+	//System.out.println("please provide divident");
+
+	//int divisor;
+	//Scanner div = new Scanner(System.in);
+	//divisor = div.nextInt();
+	//System.out.println("please provide divisor");
+	
+	//int quotient = dividend / divisor;
+	//int remainder = dividend % divisor;
+	//System.out.println("Quotient = " + quotient);
+	//System.out.println("Remainder = " + remainder);
+	}
+	}
